d095b4ddbd6d77dc2d76093c3999b31e7a05b2e9
Pierre Lambert <pierrelambert41@gmail.com>
2018-01-23 21:50:48 +0100

    ajout controller+vue section about

--- src/AppBundle/Controller/AboutController.php
+++ src/AppBundle/Controller/AboutController.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: pierre
+ * Date: 23/01/18
+ * Time: 21:29
+ */
 
 namespace AppBundle\Controller;
 
@@ -7,18 +13,19 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class AboutController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
 
         $personals = $em->getRepository(Personal::class)->findAll();
-        return $this->render('default/index.html.twig', array(
+        return $this->render('default/about.html.twig', array(
             'personals' => $personals,
         ));
     }
+
 }
