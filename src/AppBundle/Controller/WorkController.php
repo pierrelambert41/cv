04e6e365043d11cccdfb88a8ba44c8dd7e78c73a
Pierre Lambert <pierrelambert41@gmail.com>
2018-01-23 23:52:21 +0100

    ajout controler+view section work

--- src/AppBundle/Controller/WorkController.php
+++ src/AppBundle/Controller/WorkController.php
@@ -3,32 +3,29 @@
  * Created by PhpStorm.
  * User: pierre
  * Date: 23/01/18
- * Time: 21:29
+ * Time: 21:53
  */
 
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Experience;
-use AppBundle\Entity\Personal;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class AboutController extends Controller
+class WorkController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/")
      */
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
 
-        $personals = $em->getRepository(Personal::class)->findAll();
         $experiences = $em->getRepository(Experience::class)->findAll();
-        return $this->render('default/work.html.twig', [
-            'personals' => $personals,
+        return $this->render('default/work.html.twig', array(
             'experiences' => $experiences,
-        ]);
+        ));
     }
 
 }
--- src/AppBundle/Repository/ExperienceRepository.php
+++ src/AppBundle/Repository/ExperienceRepository.php
@@ -10,4 +10,8 @@ namespace AppBundle\Repository;
  */
 class ExperienceRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function findAll()
+    {
+        return $this->findBy(array(), array('id' => 'DESC'));
+    }
 }
